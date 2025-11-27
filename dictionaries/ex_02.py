3358a12ef43815cc6caa2c4832a6668387b7eb0c
norberMV <norbermv.dev@gmail.com>
2025-11-26 20:55:59 -0500

    Character counter dict

---
+++ dictionaries/ex_02.py
@@ -0,0 +1,17 @@
+"""Counting the character appearance in a string"""
+import json
+
+
+def character_counter(text):
+    counts = dict()
+    
+    for char in text:
+        counts[char] = counts.get(char, 0) + 1
+
+    return counts
+
+
+if __name__ == "__main__":
+    text = "There can be no covenants between men and lions."
+    counter_dict = character_counter(text)
+    print(json.dumps(counter_dict, ensure_ascii=False, indent=4))
